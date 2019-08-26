f8d2125a13daa32dcb00183740d02821969cb85c
HenryH <52370186+Zikun-Huang@users.noreply.github.com>
2019-08-26 16:56:52 +0800

    package for release mode

--- HelloWorld/App.xaml.cs
+++ HelloWorld/App.xaml.cs
@@ -5,6 +5,9 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
 
 namespace HelloWorld
 {
@@ -13,5 +16,11 @@ namespace HelloWorld
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            AppCenter.Start("36cbe0cb-a996-4a2a-b57a-fc0cc64227ee",
+                   typeof(Analytics), typeof(Crashes));
+        }
     }
 }
